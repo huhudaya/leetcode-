7916068116e303574ea7b3607bd21036a207d1cd
hukaida <457775600@qq.com>
2020-03-13 22:51:12 +0800

    format

---
+++ 229. 求众数 II.py
@@ -0,0 +1,77 @@
+'''
+给定一个大小为 n 的数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+
+说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1)。
+
+示例 1:
+
+输入: [3,2,3]
+输出: [3]
+示例 2:
+
+输入: [1,1,1,3,3,2,2,2]
+输出: [1,2]
+'''
+# 摩尔投票算法
+from typing import List
+
+# 最多输出两个数， 反正法：如果超过3个，那么不满足题意！
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        candiate1 = candiate2 = None
+        cnt1 = cnt2 = 0
+        for num in nums:
+            if num == candiate1:
+                cnt1 += 1
+            elif num == candiate2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                candiate1 = num
+                # cnt1 = 1
+                cnt1 += 1
+            elif cnt2 == 0:
+                candiate2 = num
+                # cnt2 = 1
+                cnt2 += 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        '''
+        最后会有这么几种可能：有2个大于n/3，有1个大于n/3，有0个大于n/3
+        遍历结束后选出了两个候选人，但是这两个候选人是否满足>n/3
+        还需要再遍历一遍数组，找出两个候选人的具体票数，因为题目没有像169题保证一定有一个大于一半的
+        '''
+        return [n for n in (candiate1, candiate2) if nums.count(n) > len(nums) // 3]
+
+# 自己的版本
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        # 摩尔投票法 最多有两个数超过n//3
+        cnt1 = 0
+        cnt2 = 0
+        candiate1 = None
+        candiate2 = None
+        res = []
+        size = len(nums) // 3
+        for num in nums:
+            if num == candiate1:
+                cnt1 += 1
+            elif num == candiate2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                candiate1 = num
+                cnt1 += 1
+            elif cnt2 == 0:
+                candiate2 = num
+                cnt2 += 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        p1 = nums.count(candiate1)
+        p2 = nums.count(candiate2)
+        if p1 > size:
+            res.append(candiate1)
+        if p2 > size:
+            res.append(candiate2)
+        return res
+        # return [n for n in (candiate1, candiate2) if nums.count(n) > len(nums) // 3]
---
+++ 347. 前 K 个高频元素.py
@@ -0,0 +1,99 @@
+'''
+给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+
+示例 1:
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+
+示例 2:
+输入: nums = [1], k = 1
+输出: [1]
+说明：
+
+你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+'''
+# 1排序
+# O(Nlog(N))
+
+
+# 堆排序
+import heapq
+import collections
+
+
+# 建立堆，堆中添加一个元素的复杂度是 O(log(k))，要进行 N 次复杂度是 O(N)。
+# counter方法复杂度为O（N），建堆和输出结果是O（NlogK）
+# Klog(K) 空间复杂度为O(N)
+class Solution:
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        #  count是map， map.get(key)得到value， value表示的是统计元素出现的个数， 这里是实现一个自定义比较器
+        count = collections.Counter(nums)
+        return heapq.nlargest(k, count.keys(), key=count.get)
+
+
+# 桶排序
+# 时间复杂度O(N),空间复杂度O(N)
+'''
+//基于桶排序求解「前 K 个高频元素」
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> res = new ArrayList();
+        // 使用字典，统计每个元素出现的次数，元素为键，元素出现的次数为值
+        HashMap<Integer,Integer> map = new HashMap();
+        for(int num : nums){
+            if (map.containsKey(num)) {
+               map.put(num, map.get(num) + 1);
+             } else {
+                map.put(num, 1);
+             }
+        }
+        
+        //桶排序
+        //将频率作为数组下标，对于出现频率不同的数字集合，存入对应的数组下标
+        List<Integer>[] list = new List[nums.length+1];
+        for(int key : map.keySet()){
+            // 获取出现的次数作为下标
+            int i = map.get(key);
+            if(list[i] == null){
+               list[i] = new ArrayList();
+            } 
+            list[i].add(key);
+        }
+        
+        // 倒序遍历数组获取出现顺序从大到小的排列
+        for(int i = list.length - 1;i >= 0 && res.size() < k;i--){
+            if(list[i] == null) continue;
+            res.addAll(list[i]);
+        }
+        return res;
+    }
+}
+'''
+from typing import List
+
+import collections
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if nums is None:
+            return
+        n = len(nums)
+        # 使用桶排序
+        bucket = [[] for _ in range(n + 1)]
+        res = []
+        cnt = 0
+        hash = collections.Counter(nums)
+        for key in hash:
+            bucket[hash.get(key)].append(key)
+        # 倒着遍历bucket
+        for num in bucket[::-1]:
+            if cnt == k:
+                return res
+            if num:
+                res.extend(num)
+                cnt += len(num)
